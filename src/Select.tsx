d68f872e20d694f07d97af415e2c03ecd301052b
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-12-10 10:24:58 +0800

    a simple selector field in typescript

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Select.module.css';
 
 type SelectOption = {
@@ -11,16 +12,48 @@ type SelectProps = {
   onChange: (value: SelectOption | undefined) => void;
 };
 
-export default function select({ value, oncChange, options }: SelectProps) {
+export default function Select({ value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function clearOptions() {
+    onChange(undefined);
+  }
+
+  function selectOption(option: SelectOption) {
+    onChange(option);
+  }
+
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>value</span>
-      <button className={styles['clear-btn']}>&times;</button>
+    <div
+      onBlur={() => setIsOpen(false)}
+      onClick={() => setIsOpen((prev) => !prev)}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+
+          clearOptions();
+        }}
+        className={styles['clear-btn']}
+      >
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={styles.options}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ''}`}>
         {options.map((option) => (
-          <li className={styles.option} key={option.label}>
+          <li
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            className={styles.option}
+            key={option.label}
+          >
             {option.label}
           </li>
         ))}
